35cb6d59b4e6b662ea4ee51c2b3c31b6489535fe
yuvalSoffer1 <yuval@soffer.name>
2025-09-03 13:41:10 +0300

    simon fix

--- assets/JS/simon.js
+++ assets/JS/simon.js
@@ -5,6 +5,13 @@ var randomChosenColour="";
 var userClickedPattern = [];
 var started = false;
 var level = 0;
+$("h1").click(function(){
+    if(!started){
+        $("#level-title").text("Level "+level);
+        nextSequence();
+        started = true;
+    }
+});
 $(document).keypress(function(){
     if(!started){
         $("#level-title").text("Level "+level);
